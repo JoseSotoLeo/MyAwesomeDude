19520520eefef625d8e4c4daed41a505ecd78147
joseSoto <jose.soto@zestspain.com>
2021-07-22 00:53:12 +0200

    updates to deploy in heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/awesomeDudeHelper'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/awesomeDudeHelper/index.html'));
+res.sendFile(path.join(__dirname+'/dist/awesomeDudeHelper/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
